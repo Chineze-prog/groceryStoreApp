094f9f41f67499df4c180eb6652dbcf620d11606
Chineze-prog <chinezeobi.bomi@gmail.com>
2024-07-05 14:15:05 -0600

    implementing backend code for products

--- backend/productsDao.py
+++ backend/productsDao.py
@@ -11,7 +11,6 @@ def get_all_products(sql_connection):
     )
 
     cursor.execute(query)
-
     response = []
 
     for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
@@ -23,12 +22,50 @@ def get_all_products(sql_connection):
             "uom_name": uom_name
         })
 
-
     return response
+
+
+def insert_new_product(sql_connection, product):
+    cursor = sql_connection.cursor()
+
+    #handling parameterized queries
+    query = (
+        "INSERT INTO grocerystoreschema.products (name, uom_id, price_per_unit) "
+        "VALUES (%s, %s, %s)"
+    )
+
+    data = (product["product_name"], product["uom_id"], product["price_per_unit"])
+    cursor.execute(query, data)
+    sql_connection.commit()
+    return cursor.lastrowid #returns the row_id of the last row that was inserted
+
+
+def delete_product(sql_connection, product_id):
+    cursor = sql_connection.cursor()
+    query = ("DELETE FROM grocerystoreschema.products WHERE product_id = " + str(product_id))
+    cursor.execute(query)
+    sql_connection.commit()
 
 
 #making the output modular
 if __name__ == "__main__":
     connection = get_sql_connection()
+
     print(get_all_products(connection))
+
+    print(delete_product(connection, 14))
+
     connection.close()
+
+"""
+    print(
+        insert_new_product(
+            connection,
+            {
+                "product_name": "cabbage",
+                "uom_id": 1,
+                "price_per_unit": 10,
+            }
+        )
+    )
+"""
